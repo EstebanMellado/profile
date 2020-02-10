49d5ce4e9a7ddc0689591b18ddeae2ed1bad6c7f
esteban <melladoesteban@yahoo.com.ar>
2020-02-10 20:57:05 -0300

    add skills

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 My Web Page Profile
 
 Made with Vuejs and (L)
+
+# ToDos
+- Add icons/images of technologies i use
+- Animate those icons/images
+- Add links (also as images) to my social networks
+- Add English and Spanish version
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,46 +2,114 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
+      For a guide and recipes on how to configure / customize this project,
+      <br />check out the
+      <a
+        href="https://cli.vuejs.org"
+        target="_blank"
+        rel="noopener"
+      >vue-cli documentation</a>.
     </p>
     <h3>Some of my skills</h3>
-    <p>TODO: try to use icons/images</p>
     <ul>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
+        <a href="https://dotnet.microsoft.com/" target="_blank" rel="noopener">
+          <b-img
+            :src="require('../assets/netCoreLogo.svg.png')"
+            fluid
+            alt="Net Core Logo"
+            width="50"
+            height="50"
+          ></b-img>
+        </a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
+        <a href="https://nodejs.org/es/" target="_blank" rel="noopener">
+          <b-img
+            :src="require('../assets/nodeLogo.jpg')"
+            fluid
+            alt="Node.js Logo"
+            width="70"
+            height="70"
+          ></b-img>
+        </a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
+        <a href="https://vuejs.org/" target="_blank" rel="noopener">
+          <b-img
+            :src="require('../assets/vueLogo.png')"
+            fluid
+            alt="Vue.js Logo"
+            width="50"
+            height="50"
+          ></b-img>
+        </a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
+        <a href="https://angular.io/" target="_blank" rel="noopener">
+          <b-img
+            :src="require('../assets/angularLogo.svg.png')"
+            fluid
+            alt="Angular Logo"
+            width="70"
+            height="70"
+          ></b-img>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.mongodb.com/" target="_blank" rel="noopener">
+          <b-img
+            :src="require('../assets/mongodbLogo.png')"
+            fluid
+            alt="Mongodb Logo"
+            width="70"
+            height="70"
+          ></b-img>
+        </a>
+      </li>
+      <li>
+        <a href="https://en.wikipedia.org/wiki/SQL" target="_blank" rel="noopener">
+          <b-img
+            :src="require('../assets/sqlLogo.png')"
+            fluid
+            alt="SQL Logo"
+            width="80"
+            height="80"
+          ></b-img>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.docker.com/" target="_blank" rel="noopener">
+          <b-img
+            :src="require('../assets/dockerLogo.png')"
+            fluid
+            alt="Docker Logo"
+            width="70"
+            height="70"
+          ></b-img>
+        </a>
+      </li>
+      <li>
+        <a href="https://azure.microsoft.com/es-es/" target="_blank" rel="noopener">
+          <b-img
+            :src="require('../assets/azureLogo.png')"
+            fluid
+            alt="Azure Logo"
+            width="70"
+            height="70"
+          ></b-img>
+        </a>
+      </li>
+      <li>
+        <a href="https://aws.amazon.com/" target="_blank" rel="noopener">
+          <b-img
+            :src="require('../assets/awsLogo.svg.png')"
+            fluid
+            alt="AWS Logo"
+            width="70"
+            height="70"
+          ></b-img>
+        </a>
       </li>
     </ul>
     <h3>Essential Links</h3>
@@ -51,19 +119,13 @@
         <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
       </li>
       <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a>
       </li>
       <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a>
       </li>
       <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a>
       </li>
       <li>
         <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
@@ -72,9 +134,7 @@
     <h3>Ecosystem</h3>
     <ul>
       <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
+        <a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a>
       </li>
       <li>
         <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
@@ -84,21 +144,13 @@
           href="https://github.com/vuejs/vue-devtools#vue-devtools"
           target="_blank"
           rel="noopener"
-          >vue-devtools</a
-        >
+        >vue-devtools</a>
       </li>
       <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
+        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a>
       </li>
     </ul>
   </div>
