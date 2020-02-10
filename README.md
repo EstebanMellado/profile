30f981f9f3c5e42f5d886a30aa8f7d597257e337
esteban <melladoesteban@yahoo.com.ar>
2020-02-10 00:41:09 -0300

    init project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # profile
 My Web Page Profile
+
+Made with Vuejs and (L)
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <b-alert show>
+      <b-icon icon="info-fill" scale="2" variant="info" class="mx-2"></b-icon>
+      Site under construction!
+      <b-icon icon="tools"></b-icon>
+    </b-alert>
+    <b-img
+      :src="require('../assets/steve.png')"
+      fluid
+      alt="Esteban image profile"
+      rounded="circle"
+      width="300" height="300"
+    ></b-img>
+    <HelloWorld msg="Welcome to Esteban's profile" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
