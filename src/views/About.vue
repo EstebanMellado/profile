9a221a2325d3dfae7d84f2b6acdbc0a05df1c765
esteban <melladoesteban@yahoo.com.ar>
2020-02-13 22:31:55 -0300

    style in columns

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,9 +13,19 @@
       height="300"
     ></b-img>
     <h1>Hola, soy Esteban</h1>
-    <p>Nacido en el 90 y codeando desde 2017. Siempre trabajé en el mundo IT. Motivado por nuevo desafíos, y siempre con ganas de aprender nuevas tecnologías.</p>
-    <p>Cuando no estoy codeando, estoy haciendo algun deporte o juntándome con amigos o yendo a meetups para hacer networking y conocer a otros desarrolladores.</p>
-    <p>En 2018 me convertí en orador en algunos eventos nacionales e internacionales de tecnología, esto despertó algo nuevo en mí y me dió maravillosas experiencias arriba del escenario compartiendo lo que sé.</p>
+    <div class="container">
+      <div class="row">
+        <div class="column">
+          <p>Nacido en el 90 y codeando desde 2017. Siempre trabajé en el mundo IT. Motivado por nuevos desafíos, y siempre con ganas de aprender nuevas tecnologías.</p>
+        </div>
+        <div class="column">
+          <p>Cuando no estoy codeando, estoy haciendo algún deporte o juntándome con amigos o yendo a meetups para hacer networking y conocer a otros desarrolladores.</p>
+        </div>
+        <div class="column">
+          <p>En 2019 me convertí en orador en algunos eventos nacionales e internacionales de tecnología, esto despertó algo nuevo en mí y me dió maravillosas experiencias arriba del escenario compartiendo lo que sé.</p>
+        </div>
+      </div>
+    </div>
 
     <h3>Algunos de mis conocimientos</h3>
     <Skills />
@@ -41,6 +51,29 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
+}
+* {
+  box-sizing: border-box;
+}
+
+/* Create three equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 10px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+  }
 }
 </style>
   
