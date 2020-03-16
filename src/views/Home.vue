89a0282e51219fcb1a3902812397c89195d3ca15
Esteban Mellado <melladoestebanjr@gmail.com>
2020-03-16 00:38:35 -0300

    add scroll event listener

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
-    <b-img
-      :src="require('../assets/steve.png')"
-      fluid
-      alt="Esteban image profile"
-      rounded="circle"
-      width="300"
-      height="300"
-    ></b-img>
+    <div id="profileImage"
+    class="fondo"
+        v-bind:class="{ hideImage: isActive }">
+      <b-img
+        :src="require('../assets/steve.png')"
+        fluid
+        alt="Esteban image profile"
+        rounded="circle"
+        width="300"
+        height="300"
+      ></b-img>
+    </div>
     <HelloWorld msg="Welcome to Esteban's profile" />
   </div>
 </template>
@@ -20,6 +24,37 @@ export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      isActive: false,
+      hideImage: 'image-mobile'
+    };
+  },
+  mounted() {
+    this.$nextTick(function() {
+      window.addEventListener("scroll", function() {
+        if (document.documentElement.scrollTop >= 100) {
+          this.isActive = true;
+          console.log("ocultar imagen, mostrar navbar");
+          console.log(this.isActive);
+        } else {
+          this.isActive = false;
+          console.log("mostrar imagen, ocultar navbar");
+          console.log(this.isActive);
+        }
+      });
+    });
   }
 };
 </script>
+
+<style>
+.image-mobile {
+  /* display: none; */
+  background-color: blue;
+}
+.fondo{
+  background-color: blueviolet;
+}
+</style>
